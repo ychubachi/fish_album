cd2acb6813d96cf1aeb707f53a94193e4a146d9c
Yoshihide Chubachi <yoshi@chubachi.net>
2011-11-25 09:12:28 +0900

    i18n, validation

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,2 +1,17 @@
+# -*- coding: utf-8 -*-
+require 'nkf'
+
 class Fish < ActiveRecord::Base
+  include NKF
+
+  before_validation do |fish|
+    logger.debug 'before_validation'
+    fish.family_jp = nkf('-w --katakana', fish.family_jp)
+    fish.family_jp = fish.family_jp.tr('科','')
+    fish.name_jp = nkf('-w --katakana', fish.name_jp)
+    fish.nickname_jp = nkf('-w --katakana', fish.nickname_jp)
+    fish.family_en = fish.family_en.capitalize
+    fish.name_en = fish.name_en.capitalize
+    fish.name_la = fish.name_la.capitalize
+  end
 end
