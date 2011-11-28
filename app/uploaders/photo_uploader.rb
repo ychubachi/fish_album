2804f75f17e0f48c244afbb5df3f26bdf254563b
Yoshihide Chubachi <yoshi@chubachi.net>
2011-11-29 01:31:15 +0900

    Thumbnail

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -32,19 +32,19 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [200,200]
+    process :resize_to_fill => [50,50]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "fish_photo.jpg" if original_filename
+  end
 
 end
