6e510fca182f8da2ccc1fef3fc3b1b15440bf8f5
Yoshihide Chubachi <yoshi@chubachi.net>
2011-11-30 14:45:52 +0900

    Settings

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -2,4 +2,12 @@ class MobileController < ApplicationController
   def index
   end
 
+  def settings
+    puts env('HTTP_METHOD')
+  end
+
+  def update_settings
+    puts params.to_yaml
+    redirect_to '/'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 FishAlbum::Application.routes.draw do
-  get "admin/login"
-
-  get "admin/logout"
+  get  'mobile/index'
+  get  'mobile/settings'
+  post 'mobile/settings'
+  get  "admin/login"
+  get  "admin/logout"
 
   resources :families do
     resources :fish do
       resources :photos
     end
   end
-  root to: "families#index"
+  root to: "mobile#index"
 end
