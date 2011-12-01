2f44f0ea370f2a391ab212ec7d071b098f486aee
Yoshihide Chubachi <yoshi@chubachi.net>
2011-12-01 14:36:02 +0900

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
+RAILS_ENV=production
+
 cd /home/yc/work/fish_album
 git checkout work
-rake assets:precompile
 git add .
 git commit -a -m 'Deploy'
 git checkout master
 git merge work
 git push
 git checkout work
-
+# Deploy
+cd /home/rails/fish_album
+git pull
+rake db:migration
+rake assets:precompile
+touch tmp/restart.txt
+# Done
+cd /home/yc/work/fish_album
