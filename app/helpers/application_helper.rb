2392d5405a88155f6970e7923bfced57f452822c
Yoshihide Chubachi <yoshi@chubachi.net>
2011-12-19 23:40:00 +0900

    ナビゲーションを整理しました。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,3 +3,12 @@ module ApplicationHelper
     return session[:admin] == true
   end
 end
+
+module ActionView
+  class OutputBuffer < ActiveSupport::SafeBuffer
+    def <<(value)
+      super(value.to_s.force_encoding('UTF-8'))
+    end
+    alias :append= :<<
+  end
+end
